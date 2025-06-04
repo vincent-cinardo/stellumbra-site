ee86dfffc2ff614dcc895a542631b47dee03e7ad
vincent-cinardo <vcinardo@gmail.com>
2025-06-04 17:58:59 -0400

    Reusable Quill RTE.

---
+++ StellumbraSite/Client/wwwroot/js/quillInterop.js
@@ -0,0 +1,17 @@
+﻿window.quillEditor = {
+    instance: null,
+
+    initialize: function (elementId) {
+        this.instance = new Quill('#' + elementId, {
+            theme: 'snow'
+        });
+    },
+
+    getContent: function () {
+        return this.instance.root.innerHTML;
+    },
+
+    setContent: function (html) {
+        this.instance.root.innerHTML = html;
+    }
+};
